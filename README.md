76d28c8ddca79e48944c3068592d7b0235df4dac
Karen Palacio <karen.palacio.1994@gmail.com>
2021-04-01 20:08:47 -0300

    rerouter to send broswer osc messages to wekinator. wekinator project for p5 to blender!

--- README.md
+++ README.md
@@ -23,7 +23,10 @@ to use any example
 ## ➼ Inputs:
 - `sender.py`: simple 1 input sender. Sends a number to wekinator.
 - `p5_examples/sketch.js`: 1 input sender. Sends mouseX as a string (WIP) (TODO: pls make it  a float)
+- `p5_examples/re_router.py`: Receives p5 inputs from the broswer (through OSC/websockets) and sends them back to wekinator (through OSC/udp), mantaining channel information. Based on `p5_examples/server_websocket_osc.py`
+
 ## ↜ Outputs:
 - `server.py` : simple 1 output osc server to use as template. Just prints the recieved float, which can be a number from 0 to 1.
 - `window.py`: waits for 3 outputs from wekinator. Takes 3 floats from [0,1] and transforms each to [0,256] int values, and draws the resulting color on a window!
-- `p5_examples/server_websocket_osc.py`: 1 output osc server through websockets to listen to a p5 client. A template for future p5 integrations. (WIP)
+- `p5_examples/server_websocket_osc.py`: 1 output osc server through websockets to listen to a p5 client. A template for future p5 integrations, and not a real weki output. (WIP)
+
---
+++ p5_examples/re_router.py
@@ -0,0 +1,34 @@
+from simple_websocket_server import WebSocketServer, WebSocket
+import struct
+encoding = 'windows-1252'
+
+from pythonosc import udp_client
+
+p5_port = 8081
+weki_port = 6448
+client = udp_client.SimpleUDPClient('127.0.0.1', weki_port)
+
+class SimpleEcho(WebSocket):
+    def handle(self):
+        # osc channel
+        channel = self.data[0:5].decode('utf-8')
+        # message type
+        msg_type = self.data[9:10].decode('utf-8')
+        # get message
+        number = struct.unpack('>f', self.data[12:])[0]
+        print("msg from "+channel+ ". type: "+msg_type +", with value:>>", round(number,2))
+        # from p5 to weki
+        print("Sending to weki ", channel)
+        client.send_message(channel, round(float(number),2))
+
+    def connected(self):
+        print(self.address, 'connected')
+
+    def handle_close(self):
+        print(self.address, 'closed')
+
+while True:
+    print(f"[*] I listen inputs from p5 in localhost:{p5_port}")
+    print(f"[*] I reroute msgs to wekinator, in localhost:{weki_port}")
+    server = WebSocketServer('', p5_port, SimpleEcho)
+    server.serve_forever()
